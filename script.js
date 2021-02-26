cc0e82dbd2850c7befbc62c8a57b29d1655f80a6
willwho08 <79634945+willwho08@users.noreply.github.com>
2021-02-26 12:33:42 +0800

    Upp

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ showSlides();
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  for (i = 1; i < slides.length; i++) {
+  for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
